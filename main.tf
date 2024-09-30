7e439ccb62422b0c2c4119708522dc41345a79e7
lucassouzasilvadev <lucasdrop169@gmail.com>
2024-09-30 20:59:34 -0300

    fix: ajuste arquivo pra subir banco rds

--- main.tf
+++ main.tf
@@ -2,41 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "rds_sg" {
-  name        = "rds-security-group"
-  description = "Allow MySQL inbound traffic"
-  
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "rds-security-group"
-  }
+data "aws_vpc" "existing_vpc" {
+  id = "vpc-0cbc562f0dbc5383c " 
 }
+
+data "aws_security_group" "existing_sg" {
+  id = "sg-0346ec3bc4f8e24d5" 
+}
+
 
 resource "aws_db_instance" "rds_mysql" {
   allocated_storage    = 20
   engine               = "mysql"
   engine_version       = "8.0.35" 
   instance_class       = "db.t4g.micro"
-  db_name              = "fiap" 
+  db_name              = "fiap"
   username             = "admin" 
   password             = "admin12345678"
   parameter_group_name = "default.mysql8.0"
   publicly_accessible  = true
   skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+  db_subnet_group_name = "rds-challenge"
   tags = {
     Name = "rds instance"
   }
